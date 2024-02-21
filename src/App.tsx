b575a5c7defa2bda4f96d50e1998b40117a74035
rondon1947 <rohan.nishant1999@gmail.com>
2024-02-21 13:46:10 +0530

    Adding some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import React from "react";
 import HomePage from "./components/main_page/HomePage";
-import BlogMain from "./components/projects/blog/BlogMain";
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/blog" element={<BlogMain />} />
-          </Routes>
-        </div>
-      </Router>
+      <HomePage />
   );
 }
 
